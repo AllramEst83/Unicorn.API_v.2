8f60ff8f30e8062f33de4c7e8442389dd99bb8e9
kay Wiberg <kaywib@hotmail.com>
2019-01-10 08:00:17 +0100

    created all aolutions and did npm install on React

---
+++ Public.API/Controllers/PublicUnicornController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Public.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicUnicornController : ControllerBase
+    {
+        // GET api/PublicUnicorn
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Unicorn1", "Unicorn2" };
+        }
+
+        // GET api/PublicUnicorn/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Unicorn1";
+        }
+
+        // POST api/PublicUnicorn
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/PublicUnicorn/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/PublicUnicorn/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+}
